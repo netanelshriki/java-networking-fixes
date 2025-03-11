303c7287be36d81e042767953400642a5d04a7ad
Netanel <73701183+netanelshriki@users.noreply.github.com>
2025-03-11 15:20:16 +0200

    Add HttpMethod enum file

---
+++ src/main/java/com/network/api/http/HttpMethod.java
@@ -0,0 +1,47 @@
+package com.network.api.http;
+
+/**
+ * HTTP methods supported by the client.
+ */
+public enum HttpMethod {
+    
+    /**
+     * The HTTP GET method.
+     */
+    GET,
+    
+    /**
+     * The HTTP POST method.
+     */
+    POST,
+    
+    /**
+     * The HTTP PUT method.
+     */
+    PUT,
+    
+    /**
+     * The HTTP DELETE method.
+     */
+    DELETE,
+    
+    /**
+     * The HTTP HEAD method.
+     */
+    HEAD,
+    
+    /**
+     * The HTTP OPTIONS method.
+     */
+    OPTIONS,
+    
+    /**
+     * The HTTP PATCH method.
+     */
+    PATCH,
+    
+    /**
+     * The HTTP TRACE method.
+     */
+    TRACE
+}
